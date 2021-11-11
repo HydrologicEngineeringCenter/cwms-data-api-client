a4d4d18926ccfb0603d2e7ee551a26b6d857a0b3
akorynta <adam@rmanet.com>
2021-11-11 11:25:49 -0800

    add test for multi-threaded timeseries retrieval. This is not quite a good mock of the multi-threaded environment given that it is sending requests to different mocked ports, but it is the closest I can get at this layer

--- cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/TimeSeriesController.java
+++ cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/TimeSeriesController.java
@@ -1,25 +1,8 @@
 /*
- * MIT License
- *
- * Copyright (c) 2021 Hydrologic Engineering Center
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (c) 2021
+ * United States Army Corps of Engineers - Hydrologic Engineering Center (USACE/HEC)
+ * All Rights Reserved.  USACE PROPRIETARY/CONFIDENTIAL.
+ * Source may not be released without written approval from HEC
  */
 
 package mil.army.usace.hec.cwms.radar.client.controllers;
@@ -33,8 +16,10 @@ import mil.army.usace.hec.cwms.radar.client.model.TimeSeries;
 
 public final class TimeSeriesController {
 
+    private static final String TIME_SERIES_ENDPOINT = "timeseries";
+
     public TimeSeries retrieveTimeSeries(ApiConnectionInfo apiConnectionInfo, TimeSeriesEndpointInput timeSeriesEndpointInput) throws IOException {
-        HttpRequestResponse response = new HttpRequestBuilder(apiConnectionInfo, "timeseries")
+        HttpRequestResponse response = new HttpRequestBuilder(apiConnectionInfo, TIME_SERIES_ENDPOINT)
             .addQueryHeader("accept", "application/json;version=2")
             .addEndpointInput(timeSeriesEndpointInput)
             .execute();
--- cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestTimeSeriesController.java
+++ cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestTimeSeriesController.java
@@ -1,25 +1,8 @@
 /*
- * MIT License
- *
- * Copyright (c) 2021 Hydrologic Engineering Center
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (c) 2021
+ * United States Army Corps of Engineers - Hydrologic Engineering Center (USACE/HEC)
+ * All Rights Reserved.  USACE PROPRIETARY/CONFIDENTIAL.
+ * Source may not be released without written approval from HEC
  */
 
 package mil.army.usace.hec.cwms.radar.client.controllers;
@@ -27,6 +10,7 @@ package mil.army.usace.hec.cwms.radar.client.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -36,6 +20,10 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import mil.army.usace.hec.cwms.htp.client.MockHttpServer;
 import mil.army.usace.hec.cwms.http.client.ApiConnectionInfo;
 import mil.army.usace.hec.cwms.http.client.NoDataFoundException;
@@ -60,7 +48,7 @@ class TestTimeSeriesController {
         mockHttpServer.shutdown();
     }
 
-    private ApiConnectionInfo buildConnectionInfo() {
+    private ApiConnectionInfo buildConnectionInfo(MockHttpServer mockHttpServer) {
         String baseUrl = String.format("http://localhost:%s", mockHttpServer.getPort());
         return new ApiConnectionInfo(baseUrl);
     }
@@ -68,7 +56,7 @@ class TestTimeSeriesController {
     @Test
     void testRetrieveTimeSeries() throws IOException {
         Path path = new File(getClass().getClassLoader().getResource("radar/v2/json/timeseries.json")
-            .getFile()).toPath();
+                                       .getFile()).toPath();
         String collect = String.join("\n", Files.readAllLines(path));
         mockHttpServer.enqueue(collect);
         mockHttpServer.start();
@@ -81,7 +69,7 @@ class TestTimeSeriesController {
             .begin(start)
             .end(end)
             .page(null);
-        TimeSeries timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(), input);
+        TimeSeries timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(mockHttpServer), input);
         assertEquals(500, timeSeries.getValues().size());
         assertEquals(745, timeSeries.getTotal());
         assertEquals("SWT", timeSeries.getOfficeId());
@@ -98,14 +86,13 @@ class TestTimeSeriesController {
     void testRetrieveTimeSeriesPagination() throws IOException {
         Path page1 =
             new File(getClass().getClassLoader().getResource("radar/v2/json/timeseries_page1.json")
-                .getFile()).toPath();
+                               .getFile()).toPath();
         Path page2 =
             new File(getClass().getClassLoader().getResource("radar/v2/json/timeseries_page2.json")
-                .getFile()).toPath();
+                               .getFile()).toPath();
         String page1Body = String.join("\n", Files.readAllLines(page1));
         String page2Body = String.join("\n", Files.readAllLines(page2));
         mockHttpServer.enqueue(page1Body);
-        mockHttpServer.enqueue(page2Body);
         mockHttpServer.start();
         Instant start = ZonedDateTime.of(2018, 1, 5, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
         Instant end = ZonedDateTime.of(2018, 2, 5, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
@@ -117,7 +104,8 @@ class TestTimeSeriesController {
             .end(end)
             .page(null)
             .pageSize(500);
-        TimeSeries timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(), input);
+        mockHttpServer.enqueue(page2Body);
+        TimeSeries timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(mockHttpServer), input);
         assertEquals(500, timeSeries.getValues().size());
         assertEquals(745, timeSeries.getTotal());
         assertEquals("SWT", timeSeries.getOfficeId());
@@ -130,7 +118,7 @@ class TestTimeSeriesController {
         assertTrue(end.isAfter(lastTime));
         assertEquals(start, firstTime);
         input.page(timeSeries.getNextPage());
-        timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(), input);
+        timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(mockHttpServer), input);
         assertEquals(245, timeSeries.getValues().size());
         assertEquals(745, timeSeries.getTotal());
         assertEquals("SWT", timeSeries.getOfficeId());
@@ -149,7 +137,7 @@ class TestTimeSeriesController {
     @Test
     void testCwmsRadarDown() throws IOException {
         Path path = new File(getClass().getClassLoader().getResource("radar/v2/json/timeseries.json")
-            .getFile()).toPath();
+                                       .getFile()).toPath();
         String collect = String.join("\n", Files.readAllLines(path));
         mockHttpServer.enqueue(collect);
         mockHttpServer.start();
@@ -161,7 +149,7 @@ class TestTimeSeriesController {
     @Test
     void testTimeSeriesNotFound() throws IOException {
         Path path = new File(getClass().getClassLoader().getResource("radar/v2/json/timeseries_notfound.json")
-            .getFile()).toPath();
+                                       .getFile()).toPath();
         String collect = String.join("\n", Files.readAllLines(path));
         mockHttpServer.enqueue(404, collect);
         mockHttpServer.start();
@@ -169,6 +157,62 @@ class TestTimeSeriesController {
         Instant end = ZonedDateTime.of(2018, 2, 5, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
         TimeSeriesController timeSeriesController = new TimeSeriesController();
         TimeSeriesEndpointInput input = new TimeSeriesEndpointInput("arbu.Elev.Inst.1Hour.0.bogus");
-        assertThrows(NoDataFoundException.class, () -> timeSeriesController.retrieveTimeSeries(buildConnectionInfo(), input));
+        assertThrows(NoDataFoundException.class, () -> timeSeriesController.retrieveTimeSeries(buildConnectionInfo(mockHttpServer), input));
+    }
+
+    @Test
+    void testMultiThreadedRetrieveTimeSeries() throws ExecutionException, InterruptedException {
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            int fileNumber = i % 10 + 1;
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() ->
+            {
+                try {
+
+                    Path path = new File(getClass().getClassLoader().getResource("radar/v2/json/timeseries" + fileNumber + ".json")
+                                                   .getFile()).toPath();
+                    String collect = String.join("\n", Files.readAllLines(path));
+                    MockHttpServer mockHttpServer = null;
+                    try {
+                        mockHttpServer = MockHttpServer.create();
+                        mockHttpServer.enqueue(collect);
+                        mockHttpServer.start();
+                        Instant start = ZonedDateTime.of(2018, 1, 5, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
+                        Instant end = ZonedDateTime.of(2018, 2, 5, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
+                        TimeSeriesEndpointInput input = new TimeSeriesEndpointInput("arbu.Elev.Inst.1Hour.0.Ccp-Rev")
+                            .officeId("SWT")
+                            .unit("SI")
+                            .verticalDatum("NAVD88")
+                            .begin(start)
+                            .end(end)
+                            .page(null);
+                        TimeSeries timeSeries = new TimeSeriesController().retrieveTimeSeries(buildConnectionInfo(mockHttpServer), input);
+                        assertEquals(500, timeSeries.getValues().size());
+                        assertEquals(745, timeSeries.getTotal());
+                        assertEquals("SWT", timeSeries.getOfficeId());
+                        assertEquals("m", timeSeries.getUnits());
+                        assertEquals(Duration.ZERO, timeSeries.getInterval());
+                        assertEquals("ARBU" + fileNumber + ".Elev.Inst.1Hour.0.Ccp-Rev", timeSeries.getName());
+                        assertEquals(start, timeSeries.getBegin().toInstant());
+                        Instant lastTime = Instant.ofEpochMilli(
+                            timeSeries.getValues().get(timeSeries.getValues().size() - 1).getDateTime());
+                        assertTrue(end.isAfter(lastTime));
+                    }
+                    finally {
+                        if (mockHttpServer != null) {
+                            mockHttpServer.shutdown();
+                        }
+                    }
+
+                }
+                catch (Exception e) {
+                    fail(e);
+                }
+            });
+            futures.add(future);
+        }
+        for (CompletableFuture<Void> future : futures) {
+            future.get();
+        }
     }
 }
