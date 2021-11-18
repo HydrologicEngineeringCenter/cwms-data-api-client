d1c77bf0397080ae1936ddbe45359d668e7dee17
akorynta <adam@rmanet.com>
2021-11-17 16:58:46 -0800

    add in controller for timeseries/group API

---
+++ cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/TimeSeriesGroupEndpointInput.java
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Hydrologic Engineering Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package mil.army.usace.hec.cwms.radar.client.controllers;
+
+import java.util.Optional;
+import mil.army.usace.hec.cwms.http.client.EndpointInput;
+import mil.army.usace.hec.cwms.http.client.HttpRequestBuilder;
+
+public final class TimeSeriesGroupEndpointInput extends EndpointInput {
+
+    private final String categoryId;
+    private final String groupId;
+    private String officeId;
+
+    public TimeSeriesGroupEndpointInput() {
+        this.groupId = null;
+        this.categoryId = null;
+    }
+
+    public TimeSeriesGroupEndpointInput(String categoryId, String groupId) {
+        this.categoryId = categoryId;
+        this.groupId = groupId;
+    }
+
+    public TimeSeriesGroupEndpointInput officeId(String officeId) {
+        this.officeId = officeId;
+        return this;
+    }
+
+    Optional<String> getGroupId() {
+        return Optional.ofNullable(groupId);
+    }
+
+    Optional<String> getCategoryId() {
+        return Optional.ofNullable(groupId);
+    }
+
+    @Override
+    protected HttpRequestBuilder addInputParameters(HttpRequestBuilder httpRequestBuilder) {
+        return httpRequestBuilder.addQueryParameter("office", officeId)
+                                 .addQueryParameter("category-id", categoryId)
+                                 .addQueryHeader("accept", "application/json");
+    }
+}
