41680f9dfe455d8c55b33c3ceb66dd03c2027aa3
ryan <ryan@rmanet.com>
2022-07-05 13:49:11 -0700

    Fixing checkstyle warnings.

--- cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/RatingSpecController.java
+++ cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/RatingSpecController.java
@@ -1,62 +1,60 @@
 package mil.army.usace.hec.cwms.radar.client.controllers;
 
-import java.io.IOException;
+import static mil.army.usace.hec.cwms.radar.client.controllers.RadarEndpointConstants.ACCEPT_HEADER_V2;
 
+import java.io.IOException;
 import mil.army.usace.hec.cwms.http.client.ApiConnectionInfo;
 import mil.army.usace.hec.cwms.http.client.HttpRequestBuilderImpl;
 import mil.army.usace.hec.cwms.http.client.HttpRequestResponse;
 import mil.army.usace.hec.cwms.http.client.request.HttpRequestExecutor;
-import mil.army.usace.hec.cwms.radar.client.model.RatingSpec;
 import mil.army.usace.hec.cwms.radar.client.model.RadarObjectMapper;
+import mil.army.usace.hec.cwms.radar.client.model.RatingSpec;
 import mil.army.usace.hec.cwms.radar.client.model.RatingSpecs;
 
-import static mil.army.usace.hec.cwms.radar.client.controllers.RadarEndpointConstants.ACCEPT_HEADER_V2;
 
-public class RatingSpecController
-{
-	private static final String RATING_SPEC = "ratings/spec";
+public final class RatingSpecController {
+    private static final String RATING_SPEC = "ratings/spec";
 
-	/**
-	 * Retrieve Rating Spec.
-	 *
-	 * @param apiConnectionInfo             - connection info
-	 * @param ratingSpecEndpointInput - rating-id and office
-	 * @return RatingSpec
-	 * @throws IOException - thrown if retrieve failed
-	 */
-	public RatingSpec retrieveRatingSpec(ApiConnectionInfo apiConnectionInfo, RatingSpecEndpointInput ratingSpecEndpointInput)
-			throws IOException {
-		RatingSpec retVal;
-		HttpRequestExecutor executor = new HttpRequestBuilderImpl(apiConnectionInfo,
-				RATING_SPEC + "/" + ratingSpecEndpointInput.getRatingId())
-				.addEndpointInput(ratingSpecEndpointInput)
-				.get()
-				.withMediaType(ACCEPT_HEADER_V2);
-		try (HttpRequestResponse response = executor.execute()) {
-			retVal = RadarObjectMapper.mapJsonToObject(response.getBody(), RatingSpec.class);
-		}
-		return retVal;
-	}
+    /**
+     * Retrieve Rating Spec.
+     *
+     * @param apiConnectionInfo       - connection info
+     * @param ratingSpecEndpointInput - rating-id and office
+     * @return RatingSpec
+     * @throws IOException - thrown if retrieve failed
+     */
+    public RatingSpec retrieveRatingSpec(ApiConnectionInfo apiConnectionInfo,
+                                         RatingSpecEndpointInput ratingSpecEndpointInput) throws IOException {
+        RatingSpec retVal;
+        HttpRequestExecutor executor = new HttpRequestBuilderImpl(apiConnectionInfo,
+                RATING_SPEC + "/" + ratingSpecEndpointInput.getRatingId())
+                .addEndpointInput(ratingSpecEndpointInput)
+                .get()
+                .withMediaType(ACCEPT_HEADER_V2);
+        try (HttpRequestResponse response = executor.execute()) {
+            retVal = RadarObjectMapper.mapJsonToObject(response.getBody(), RatingSpec.class);
+        }
+        return retVal;
+    }
 
-	/**
-	 * Retrieve Location Category.
-	 *
-	 * @param apiConnectionInfo             - connection info
-	 * @param ratingSpecEndpointInput - rating-id and office
-	 * @return RatingSpecs for office id
-	 * @throws IOException - thrown if retrieve failed
-	 */
-	public RatingSpecs retrieveRatingSpecs(ApiConnectionInfo apiConnectionInfo,
-	                                       RatingSpecEndpointInput ratingSpecEndpointInput)
-			throws IOException {
-		RatingSpecs retVal;
-		HttpRequestExecutor executor = new HttpRequestBuilderImpl(apiConnectionInfo, RATING_SPEC)
-				.addEndpointInput(ratingSpecEndpointInput)
-				.get()
-				.withMediaType(ACCEPT_HEADER_V2);
-		try (HttpRequestResponse response = executor.execute()) {
-			retVal = RadarObjectMapper.mapJsonToObject(response.getBody(), RatingSpecs.class);
-		}
-		return retVal;
-	}
+    /**
+     * Retrieve Location Category.
+     *
+     * @param apiConnectionInfo       - connection info
+     * @param ratingSpecEndpointInput - rating-id and office
+     * @return RatingSpecs for office id
+     * @throws IOException - thrown if retrieve failed
+     */
+    public RatingSpecs retrieveRatingSpecs(ApiConnectionInfo apiConnectionInfo,
+                                           RatingSpecEndpointInput ratingSpecEndpointInput) throws IOException {
+        RatingSpecs retVal;
+        HttpRequestExecutor executor = new HttpRequestBuilderImpl(apiConnectionInfo, RATING_SPEC)
+                .addEndpointInput(ratingSpecEndpointInput)
+                .get()
+                .withMediaType(ACCEPT_HEADER_V2);
+        try (HttpRequestResponse response = executor.execute()) {
+            retVal = RadarObjectMapper.mapJsonToObject(response.getBody(), RatingSpecs.class);
+        }
+        return retVal;
+    }
 }
--- cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/RatingSpecEndpointInput.java
+++ cwms-radar-client/src/main/java/mil/army/usace/hec/cwms/radar/client/controllers/RatingSpecEndpointInput.java
@@ -1,84 +1,75 @@
 package mil.army.usace.hec.cwms.radar.client.controllers;
 
-import java.util.Optional;
-
-import mil.army.usace.hec.cwms.http.client.EndpointInput;
-import mil.army.usace.hec.cwms.http.client.HttpRequestBuilder;
-
 import static mil.army.usace.hec.cwms.radar.client.controllers.RadarEndpointConstants.ACCEPT_HEADER_V2;
 import static mil.army.usace.hec.cwms.radar.client.controllers.RadarEndpointConstants.ACCEPT_QUERY_HEADER;
 
-public class RatingSpecEndpointInput extends EndpointInput
-{
+import java.util.Optional;
+import mil.army.usace.hec.cwms.http.client.EndpointInput;
+import mil.army.usace.hec.cwms.http.client.HttpRequestBuilder;
 
-	static final String OFFICE_QUERY_PARAMETER = "office";
-	static final String RATING_ID_MASK_QUERY_PARAMETER = "rating-id-mask";
-	static final String PAGE_QUERY_PARAMETER = "page";
-	static final String PAGE_SIZE_QUERY_PARAMETER = "page-size";
 
-	private String ratingId;
-	private String ratingIdMask;
-	private String officeId;
+public final class RatingSpecEndpointInput extends EndpointInput {
 
-	private String page;
-	private Integer pageSize;
+    static final String OFFICE_QUERY_PARAMETER = "office";
+    static final String RATING_ID_MASK_QUERY_PARAMETER = "rating-id-mask";
+    static final String PAGE_QUERY_PARAMETER = "page";
+    static final String PAGE_SIZE_QUERY_PARAMETER = "page-size";
 
-	public RatingSpecEndpointInput()
-	{
-		this.officeId = null;
-		this.ratingId = null;
-		this.ratingIdMask = null;
-	}
+    private String ratingId;
+    private String ratingIdMask;
+    private String officeId;
 
-	String getRatingIdMask()
-	{
-		return ratingIdMask;
-	}
-	String getRatingId()
-	{
-		return ratingId;
-	}
+    private String page;
+    private Integer pageSize;
 
-	public RatingSpecEndpointInput officeId(String officeId)
-	{
-		this.officeId = officeId;
-		return this;
-	}
+    public RatingSpecEndpointInput() {
+        this.officeId = null;
+        this.ratingId = null;
+        this.ratingIdMask = null;
+    }
 
-	public RatingSpecEndpointInput ratingId(String rId)
-	{
-		this.ratingId = rId;
-		return this;
-	}
+    String getRatingIdMask() {
+        return ratingIdMask;
+    }
 
-	public RatingSpecEndpointInput ratingIdMask(String rIdMask)
-	{
-		this.ratingIdMask = rIdMask;
-		return this;
-	}
+    String getRatingId() {
+        return ratingId;
+    }
 
-	public RatingSpecEndpointInput page(String page)
-	{
-		this.page = page;
-		return this;
-	}
+    public RatingSpecEndpointInput officeId(String officeId) {
+        this.officeId = officeId;
+        return this;
+    }
 
-	public RatingSpecEndpointInput pageSize(int pageSize)
-	{
-		this.pageSize = pageSize;
-		return this;
-	}
+    public RatingSpecEndpointInput ratingId(String ratingId) {
+        this.ratingId = ratingId;
+        return this;
+    }
 
-	@Override
-	protected HttpRequestBuilder addInputParameters(HttpRequestBuilder httpRequestBuilder)
-	{
-		String pageSizeString = Optional.ofNullable(pageSize).map(Object::toString).orElse(null);
+    public RatingSpecEndpointInput ratingIdMask(String ratingMask) {
+        this.ratingIdMask = ratingMask;
+        return this;
+    }
 
-		return httpRequestBuilder
+    public RatingSpecEndpointInput page(String page) {
+        this.page = page;
+        return this;
+    }
+
+    public RatingSpecEndpointInput pageSize(int pageSize) {
+        this.pageSize = pageSize;
+        return this;
+    }
+
+    @Override
+    protected HttpRequestBuilder addInputParameters(HttpRequestBuilder httpRequestBuilder) {
+        String pageSizeString = Optional.ofNullable(pageSize).map(Object::toString).orElse(null);
+
+        return httpRequestBuilder
 				.addQueryParameter(RATING_ID_MASK_QUERY_PARAMETER, ratingIdMask)
-				.addQueryParameter(OFFICE_QUERY_PARAMETER, officeId)
-				.addQueryParameter(PAGE_QUERY_PARAMETER, page)
-				.addQueryParameter(	PAGE_SIZE_QUERY_PARAMETER, pageSizeString)
-				.addQueryHeader(ACCEPT_QUERY_HEADER, ACCEPT_HEADER_V2);
-	}
+                .addQueryParameter(OFFICE_QUERY_PARAMETER, officeId)
+                .addQueryParameter(PAGE_QUERY_PARAMETER, page)
+                .addQueryParameter(PAGE_SIZE_QUERY_PARAMETER, pageSizeString)
+                .addQueryHeader(ACCEPT_QUERY_HEADER, ACCEPT_HEADER_V2);
+    }
 }
