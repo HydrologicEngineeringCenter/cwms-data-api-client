10e65cff9647a481b9944a2191dab359396b6862
Bryson Spilman <bryson@rmanet.com>
2022-09-15 15:05:53 -0700

    CWMS-1014 Updated to log nothing if string contains access_token or refresh_token

--- cwms-http-client/src/main/java/mil/army/usace/hec/cwms/http/client/CwmsHttpLogger.java
+++ cwms-http-client/src/main/java/mil/army/usace/hec/cwms/http/client/CwmsHttpLogger.java
@@ -1,17 +1,11 @@
 package mil.army.usace.hec.cwms.http.client;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 final class CwmsHttpLogger implements HttpLoggingInterceptor.Logger {
 
     private static final String ACCESS_TOKEN = "access_token";
     private static final String REFRESH_TOKEN = "refresh_token";
-    private static final Logger LOGGER = Logger.getLogger(CwmsHttpLogger.class.getName());
     private static final HttpLoggingInterceptor.Logger DELEGATE = HttpLoggingInterceptor.Logger.DEFAULT;
 
     @Override
@@ -19,26 +13,7 @@ final class CwmsHttpLogger implements HttpLoggingInterceptor.Logger {
         String lowerCaseS = s.toLowerCase();
         if (!lowerCaseS.contains(ACCESS_TOKEN.toLowerCase()) && !lowerCaseS.contains(REFRESH_TOKEN.toLowerCase())) {
             DELEGATE.log(s);
-        } else {
-            try {
-                s = redactOAuth2Tokens(s);
-                DELEGATE.log(s);
-            } catch (JsonProcessingException e) {
-                LOGGER.log(Level.INFO, () -> "Could not parse logged JSON. Redacting log containing token(s) for security reasons.");
-            }
         }
-    }
-
-    private String redactOAuth2Tokens(String s) throws JsonProcessingException {
-        ObjectMapper om = new ObjectMapper();
-        Map<String, String> jsonMap = om.readValue(s, Map.class);
-        for (Map.Entry<String, String> entry : jsonMap.entrySet()) {
-            String key = entry.getKey();
-            if (key.equalsIgnoreCase(ACCESS_TOKEN) || key.equalsIgnoreCase(REFRESH_TOKEN)) {
-                entry.setValue("<REDACTED>");
-            }
-        }
-        return om.writeValueAsString(jsonMap);
     }
 
 }
