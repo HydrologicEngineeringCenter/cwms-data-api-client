c9f59292d9d8f64099c04c977ea387925098e231
Adam Korynta <adam@rmanet.com>
2023-07-17 12:03:28 -0700

    add timing points and comparison with collected metrics

--- scripts/metrics_to_curl/to_curl.py
+++ scripts/metrics_to_curl/to_curl.py
@@ -31,6 +31,17 @@
 #  furnished to do so, subject to the following conditions:
 #
 #
+#  MIT License
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
 import csv
 import re
 import sys
@@ -45,12 +56,14 @@ def __export_curl(csv_file, output_file):
         csv_reader = csv.reader(csv_file, delimiter='|')
         line_count = 0
         path_index = 1
+        mean_index = 4
         properties_index = 20
         for row in csv_reader:
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 path_index = row.index('path')
                 properties_index = row.index('Properties')
+                mean_index = row.index('mean')
                 line_count += 1
             else:
                 url = row[path_index].replace('Timer', '')
@@ -64,12 +77,14 @@ def __export_curl(csv_file, output_file):
                 accept_header = ''
                 if result:
                     accept_header = ' --header "Accept:' + result.groups()[0] + '"'
-                file_content += 'curl -k --write-out \'%{http_code}\' --silent --output /dev/null ' \
-                                + url + accept_header + ' &\n'
+                file_content += 'curl -k --write-out \'' + url + ' took total time: %{time_total}s while metrics collected averaged: ' \
+                                + str(float(row[mean_index]) / 1000) + 's\'' \
+                                + ' --silent --output /dev/null ' \
+                                + url + accept_header + ' ;echo -e \'\' &\n'
                 line_count += 1
         print(file_content)
         with open(output_file, "w") as text_file:
-            file_content = 'echo Start Time: \ndate +\"%r\"\necho Running cURL requests\n' + file_content
+            file_content = 'echo Start Time: \ndate +\"%r\" \necho Running cURL requests\n' + file_content
             file_content += '\nwait\necho End Time\ndate +\"%r\"'
             text_file.write(file_content)
 
