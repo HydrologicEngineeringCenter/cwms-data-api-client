cef644dedde0b73581c3eb4ca8d7ddfd984fb8b5
Adam Korynta <adam@rmanet.com>
2024-05-01 08:20:59 -0700

    cache windows keystore in order to optimize performance

--- cwms-http-client/src/test/java/mil/army/usace/hec/cwms/http/client/auth/CacKeyManagerUtilTest.java
+++ cwms-http-client/src/test/java/mil/army/usace/hec/cwms/http/client/auth/CacKeyManagerUtilTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Hydrologic Engineering Center
+ * Copyright (c) 2024 Hydrologic Engineering Center
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,16 +25,38 @@
 package mil.army.usace.hec.cwms.http.client.auth;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 final class CacKeyManagerUtilTest {
+    private static final Logger LOGGER = Logger.getLogger(CacKeyManagerUtil.class.getName());
 
     @Test
     void testPivEdipiPattern() {
         assertTrue(CacKeyManagerUtil.EDIPI_PATTERN.matcher("1234567890@").matches());
         assertTrue(CacKeyManagerUtil.EDIPI_PATTERN.matcher("1234567890@").matches());
         assertFalse(CacKeyManagerUtil.EDIPI_PATTERN.matcher("123456@").matches());
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testGetCertificateAliases() throws CacCertificateException {
+        Instant start = Instant.now();
+        assertFalse(CacKeyManagerUtil.getCertificateAliases().isEmpty(), "Windows keystore aliases should not be empty");
+        Instant end1 = Instant.now();
+        Duration duration = Duration.between(start, end1);
+        LOGGER.info("First Attempt Time taken: " + duration.toMillis() + " milliseconds");
+        assertFalse(CacKeyManagerUtil.getCertificateAliases().isEmpty(), "Cached Windows keystore aliases should not be empty");
+        Instant end2 = Instant.now();
+        Duration duration2 = Duration.between(end1, end2);
+        LOGGER.info("Second Attempt Time taken: " + duration2.toMillis() + " milliseconds");
+        assertTrue(duration.toMillis() / duration2.toMillis() >= 10, "First duration should be at least one orders of magnitude longer than the second duration");
     }
 }
