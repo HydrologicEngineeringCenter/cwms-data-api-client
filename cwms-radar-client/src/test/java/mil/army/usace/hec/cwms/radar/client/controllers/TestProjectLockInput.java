aab279660452fbdd17f89fe87fb23a1ca91a9852
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-07-15 16:41:07 -0700

    Implemented RequestLock

--- cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestProjectLockInput.java
+++ cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestProjectLockInput.java
@@ -1,11 +1,14 @@
 package mil.army.usace.hec.cwms.radar.client.controllers;
 
+import static mil.army.usace.hec.cwms.radar.client.controllers.ProjectLockInput.LockRequest.REVOKE_EXISTING;
+import static mil.army.usace.hec.cwms.radar.client.controllers.ProjectLockInput.LockRequest.REVOKE_TIMEOUT;
 import static mil.army.usace.hec.cwms.radar.client.controllers.RadarEndpointConstants.ACCEPT_HEADER_V1;
 import static mil.army.usace.hec.cwms.radar.client.controllers.RadarEndpointConstants.ACCEPT_QUERY_HEADER;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import mil.army.usace.hec.cwms.radar.client.model.ProjectLock;
 import org.junit.jupiter.api.Test;
 
 
@@ -60,6 +63,41 @@ class TestProjectLockInput {
         assertThrows(NullPointerException.class, () -> ProjectLockInput.getOne(null, proj, app));
         assertThrows(NullPointerException.class, () -> ProjectLockInput.getOne(office, null, app));
         assertThrows(NullPointerException.class, () -> ProjectLockInput.getOne(office, proj, null));
+    }
+
+    @Test
+    void testRequestLockQueryRequest() {
+        MockHttpRequestBuilder mockHttpRequestBuilder = new MockHttpRequestBuilder();
+
+        String office = "SWT";
+        String projectId = "SomeProject";
+        String appId = "MockREGI";
+        ProjectLock lock = new ProjectLock()
+                .officeId(office)
+                .projectId(projectId)
+                .applicationId(appId)
+                ;
+
+        ProjectLockInput.LockRequest input = ProjectLockInput.lockRequest(lock);
+        input.addInputParameters(mockHttpRequestBuilder);
+
+        assertEquals("false", mockHttpRequestBuilder.getQueryParameter(REVOKE_EXISTING));
+        assertEquals("10", mockHttpRequestBuilder.getQueryParameter(REVOKE_TIMEOUT));
+        assertEquals(ACCEPT_HEADER_V1, mockHttpRequestBuilder.getQueryHeader(ACCEPT_QUERY_HEADER));
+
+        input = input.revokeExisting(true)
+                .revokeTimeout(11);
+        input.addInputParameters(mockHttpRequestBuilder);
+
+        assertEquals("true", mockHttpRequestBuilder.getQueryParameter(REVOKE_EXISTING));
+        assertEquals("11", mockHttpRequestBuilder.getQueryParameter(REVOKE_TIMEOUT));
+        assertEquals(ACCEPT_HEADER_V1, mockHttpRequestBuilder.getQueryHeader(ACCEPT_QUERY_HEADER));
+
+    }
+
+    @Test
+    void testRequestLockQueryRequestNulls() {
+        assertThrows(NullPointerException.class, () -> ProjectLockInput.lockRequest(null));
     }
 
 
