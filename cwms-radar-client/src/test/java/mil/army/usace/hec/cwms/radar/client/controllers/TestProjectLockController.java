ad9a414b2106ce5eb87a190d7c3a6c8dfa1d2e97
rma-rripken <89810919+rma-rripken@users.noreply.github.com>
2024-07-15 14:53:29 -0700

    Implemented GetOne/retrieveProjectLock.

--- cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestProjectLockController.java
+++ cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestProjectLockController.java
@@ -1,5 +1,6 @@
 package mil.army.usace.hec.cwms.radar.client.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -16,7 +17,7 @@ public class TestProjectLockController extends TestController {
     public static final String APPLICATION_MASK = "MockREGI";
 
     @Test
-    void testRetrieveLockRevokerRights() throws IOException {
+    void testRetrieveProjectLocks() throws IOException {
         String collect = readJsonFile("radar/v1/json/project_locks.json");
         mockHttpServer.enqueue(collect);
         mockHttpServer.start();
@@ -27,6 +28,24 @@ public class TestProjectLockController extends TestController {
         List<ProjectLock> lockRevokerRights = controller.retrieveProjectLocks(apiConnectionInfo, input);
         assertNotNull(lockRevokerRights);
         assertFalse(lockRevokerRights.isEmpty());
+    }
+
+    @Test
+    void testRetrieveProjectLock() throws IOException {
+        String collect = readJsonFile("radar/v1/json/project_lock.json");
+        mockHttpServer.enqueue(collect);
+        mockHttpServer.start();
+
+        ProjectLockController controller = new ProjectLockController();
+
+        ProjectLockInput.GetOne input = ProjectLockInput.getOne(OFFICE_MASK, PROJECT_MASK, APPLICATION_MASK);
+        ApiConnectionInfo apiConnectionInfo = buildConnectionInfo();
+        ProjectLock prjLock = controller.retrieveProjectLock(apiConnectionInfo, input);
+        assertNotNull(prjLock);
+        assertEquals("SPK", prjLock.getOfficeId());
+        assertEquals("ProjectId0", prjLock.getProjectId());
+        assertEquals("ApplicationId", prjLock.getApplicationId());
+
     }
 
 }
