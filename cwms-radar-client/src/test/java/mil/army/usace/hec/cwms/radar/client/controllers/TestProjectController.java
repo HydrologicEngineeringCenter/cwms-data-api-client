d9da445ad8fd4c0241954a353c438d05d6691251
Adam Korynta <47677856+adamkorynta@users.noreply.github.com>
2024-08-12 10:33:28 -0700

    add project status and child location implementation endpoints (#213)
    * add project status and child location implementation endpoints

--- cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestProjectController.java
+++ cwms-radar-client/src/test/java/mil/army/usace/hec/cwms/radar/client/controllers/TestProjectController.java
@@ -1,12 +1,43 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Hydrologic Engineering Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package mil.army.usace.hec.cwms.radar.client.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import mil.army.usace.hec.cwms.http.client.ApiConnectionInfo;
+import mil.army.usace.hec.cwms.http.client.MockHttpServer;
 import mil.army.usace.hec.cwms.radar.client.model.DeleteMethod;
 import mil.army.usace.hec.cwms.radar.client.model.Project;
+import mil.army.usace.hec.cwms.radar.client.model.ProjectChildLocations;
 import mil.army.usace.hec.cwms.radar.client.model.Projects;
 import mil.army.usace.hec.cwms.radar.client.model.RadarObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -41,8 +72,8 @@ class TestProjectController extends TestController {
         ProjectController controller = new ProjectController();
 
         ProjectEndpointInput.GetAll input = ProjectEndpointInput.getAll()
-                .officeId(OFFICE)
-                .projectIdMask(MASK);
+            .officeId(OFFICE)
+            .projectIdMask(MASK);
 
         ApiConnectionInfo apiConnectionInfo = buildConnectionInfo();
         Projects projects = controller.retrieveProjects(apiConnectionInfo, input);
@@ -89,5 +120,42 @@ class TestProjectController extends TestController {
 
         ApiConnectionInfo apiConnectionInfo = buildConnectionInfo();
         assertDoesNotThrow(() -> controller.deleteProject(apiConnectionInfo, input));
+    }
+
+    @Test
+    void testPublishStatusUpdate() throws Exception {
+        mockHttpServer.enqueue("");
+        mockHttpServer.start();
+        ProjectController controller = new ProjectController();
+        Instant begin = ZonedDateTime.of(2024, 3, 1, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
+        Instant end = ZonedDateTime.of(2024, 3, 2, 0, 0, 0, 0, ZoneId.of("UTC")).toInstant();
+        ProjectEndpointInput.StatusUpdate input = ProjectEndpointInput.statusUpdate("SPK", "BIGH", "GitHub")
+            .sourceId("TestProjectController")
+            .timeSeriesId("ABC.Flow.Ave.1Day.1Day.TEST")
+            .begin(begin)
+            .end(end);
+        ApiConnectionInfo apiConnectionInfo = buildConnectionInfo();
+        assertDoesNotThrow(() -> controller.publishStatusUpdate(apiConnectionInfo, input));
+        MockHttpServer.RequestWrapper request = mockHttpServer.takeRequest();
+        assertEquals(
+            "/projects/status-update/BIGH?timeseries-id=ABC.Flow.Ave.1Day.1Day.TEST&end=2024-03-02T00%3A00%3A00Z&office=SPK&begin=2024-03-01T00%3A00%3A00Z&application-id=GitHub&source-id=TestProjectController",
+            request.getPath());
+        assertEquals("POST", request.getMethod());
+    }
+
+    @Test
+    void testGetProjectChildLocations() throws Exception {
+        String collect = readJsonFile("radar/v2/json/project_children.json");
+        mockHttpServer.enqueue(collect);
+        mockHttpServer.start();
+        ProjectController controller = new ProjectController();
+        ProjectEndpointInput.GetProjectChildLocations input = ProjectEndpointInput.getProjectChildLocations("SPK")
+            .projectIdMask("T*")
+            .locationKindMask("*");
+        ApiConnectionInfo apiConnectionInfo = buildConnectionInfo();
+        ProjectChildLocations projectChildLocations = controller.getProjectChildLocations(apiConnectionInfo, input);
+        assertEquals("TestProject", projectChildLocations.getProjectId().getName());
+        assertEquals("SPK", projectChildLocations.getProjectId().getOfficeId());
+        assertFalse(projectChildLocations.getLocationsByKind().isEmpty());
     }
 }
