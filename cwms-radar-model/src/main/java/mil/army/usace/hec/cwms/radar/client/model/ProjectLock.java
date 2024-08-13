4e735d9183cede268f38e838122819e166d2db8a
Adam Korynta <adam@rmanet.com>
2024-08-13 11:16:55 -0700

    fix missing applicatoinId in project lock endpoint

--- cwms-radar-model/src/main/java/mil/army/usace/hec/cwms/radar/client/model/ProjectLock.java
+++ cwms-radar-model/src/main/java/mil/army/usace/hec/cwms/radar/client/model/ProjectLock.java
@@ -1,11 +1,33 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Hydrologic Engineering Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package mil.army.usace.hec.cwms.radar.client.model;
 
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
+import java.util.Objects;
 
 /**
  * ProjectLock
@@ -24,7 +46,7 @@ public class ProjectLock {
     private String applicationId = null;
 
     @JsonProperty("acquire-time")
-    private Date acquireTime = null;
+    private Instant acquireTime = null;
 
     @JsonProperty("session-user")
     private String sessionUser = null;
@@ -77,16 +99,16 @@ public class ProjectLock {
         this.applicationId = applicationId;
     }
 
-    public ProjectLock acquireTime(Date acquireTime) {
+    public ProjectLock acquireTime(Instant acquireTime) {
         this.acquireTime = acquireTime;
         return this;
     }
 
-    public Date getAcquireTime() {
+    public Instant getAcquireTime() {
         return acquireTime;
     }
 
-    public void setAcquireTime(Date acquireTime) {
+    public void setAcquireTime(Instant acquireTime) {
         this.acquireTime = acquireTime;
     }
 
