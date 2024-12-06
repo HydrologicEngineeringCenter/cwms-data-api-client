20e08a0ede7cf743b6b7fec983b3a66e2de44012
zack-rma <zack@rmanet.com>
2024-12-06 13:03:51 -0800

    Updated AssignedTimeSeries DTO (#245)

--- cwms-radar-model/src/main/java/mil/army/usace/hec/cwms/radar/client/model/AssignedTimeSeries.java
+++ cwms-radar-model/src/main/java/mil/army/usace/hec/cwms/radar/client/model/AssignedTimeSeries.java
@@ -1,34 +1,7 @@
-/*
- * MIT License
- *
- * Copyright (c) 2024 Hydrologic Engineering Center
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package mil.army.usace.hec.cwms.radar.client.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.annotation.Generated;
-import jakarta.validation.Valid;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -36,8 +9,13 @@ import java.util.Objects;
  * AssignedTimeSeries
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-08-09T09:29:33.859-07:00[America/Los_Angeles]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen",
+        date = "2024-12-06T12:43:40.669367400-08:00[America/Los_Angeles]")
 public class AssignedTimeSeries {
+
+    @JsonProperty("office-id")
+    private String officeId = null;
+
     @JsonProperty("timeseries-id")
     private String timeseriesId = null;
 
@@ -47,22 +25,29 @@ public class AssignedTimeSeries {
     @JsonProperty("alias-id")
     private String aliasId = null;
 
-    @JsonProperty("ref-ts-id")
+    @JsonProperty("refTs-id")
     private String refTsId = null;
 
     @JsonProperty("attribute")
     private Integer attribute = null;
 
+    public AssignedTimeSeries officeId(String officeId) {
+        this.officeId = officeId;
+        return this;
+    }
+
+    public String getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(String officeId) {
+        this.officeId = officeId;
+    }
+
     public AssignedTimeSeries timeseriesId(String timeseriesId) {
         this.timeseriesId = timeseriesId;
         return this;
     }
-
-    /**
-     * Get timeseriesId
-     *
-     * @return timeseriesId
-     **/
 
     public String getTimeseriesId() {
         return timeseriesId;
@@ -77,13 +62,6 @@ public class AssignedTimeSeries {
         return this;
     }
 
-    /**
-     * Get tsCode
-     *
-     * @return tsCode
-     **/
-
-    @Valid
     public BigDecimal getTsCode() {
         return tsCode;
     }
@@ -96,12 +74,6 @@ public class AssignedTimeSeries {
         this.aliasId = aliasId;
         return this;
     }
-
-    /**
-     * Get aliasId
-     *
-     * @return aliasId
-     **/
 
     public String getAliasId() {
         return aliasId;
@@ -116,12 +88,6 @@ public class AssignedTimeSeries {
         return this;
     }
 
-    /**
-     * Get refTsId
-     *
-     * @return refTsId
-     **/
-
     public String getRefTsId() {
         return refTsId;
     }
@@ -135,12 +101,6 @@ public class AssignedTimeSeries {
         return this;
     }
 
-    /**
-     * Get attribute
-     *
-     * @return attribute
-     **/
-
     public Integer getAttribute() {
         return attribute;
     }
@@ -149,9 +109,8 @@ public class AssignedTimeSeries {
         this.attribute = attribute;
     }
 
-
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(java.lang.Object o) {
         if (this == o) {
             return true;
         }
@@ -159,19 +118,29 @@ public class AssignedTimeSeries {
             return false;
         }
         AssignedTimeSeries assignedTimeSeries = (AssignedTimeSeries) o;
-        return this.timeseriesId == null || assignedTimeSeries.timeseriesId == null ?
-            Objects.equals(this.timeseriesId, assignedTimeSeries.timeseriesId) :
-            this.timeseriesId.equalsIgnoreCase(assignedTimeSeries.timeseriesId) && Objects.equals(this.tsCode, assignedTimeSeries.tsCode) &&
-                this.aliasId == null || assignedTimeSeries.aliasId == null ? Objects.equals(this.aliasId, assignedTimeSeries.aliasId) :
-                this.aliasId.equalsIgnoreCase(assignedTimeSeries.aliasId) && this.refTsId == null || assignedTimeSeries.refTsId == null ?
-                    Objects.equals(this.refTsId, assignedTimeSeries.refTsId) :
-                    this.refTsId.equalsIgnoreCase(assignedTimeSeries.refTsId) && Objects.equals(this.attribute, assignedTimeSeries.attribute);
+        return this.officeId == null || assignedTimeSeries.officeId == null
+                ? Objects.equals(this.officeId, assignedTimeSeries.officeId)
+                : this.officeId.equalsIgnoreCase(assignedTimeSeries.officeId)
+                && this.timeseriesId == null || assignedTimeSeries.timeseriesId == null
+                ? Objects.equals(this.timeseriesId, assignedTimeSeries.timeseriesId)
+                : this.timeseriesId.equalsIgnoreCase(assignedTimeSeries.timeseriesId)
+                && Objects.equals(this.tsCode, assignedTimeSeries.tsCode)
+                && this.aliasId == null || assignedTimeSeries.aliasId == null
+                ? Objects.equals(this.aliasId, assignedTimeSeries.aliasId)
+                : this.aliasId.equalsIgnoreCase(assignedTimeSeries.aliasId)
+                && this.refTsId == null || assignedTimeSeries.refTsId == null
+                ? Objects.equals(this.refTsId, assignedTimeSeries.refTsId)
+                : this.refTsId.equalsIgnoreCase(assignedTimeSeries.refTsId)
+                && Objects.equals(this.attribute, assignedTimeSeries.attribute)
+                ;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(timeseriesId == null ? 0 : timeseriesId.toLowerCase(), tsCode, aliasId == null ? 0 : aliasId.toLowerCase(),
-            refTsId == null ? 0 : refTsId.toLowerCase(), attribute);
+        return Objects.hash(officeId == null ? 0 : officeId.toLowerCase(),
+                timeseriesId == null ? 0 : timeseriesId.toLowerCase(), tsCode,
+                aliasId == null ? 0 : aliasId.toLowerCase(),
+                refTsId == null ? 0 : refTsId.toLowerCase(), attribute);
     }
 
     @Override
@@ -179,6 +148,7 @@ public class AssignedTimeSeries {
         StringBuilder sb = new StringBuilder();
         sb.append("class AssignedTimeSeries {\n");
 
+        sb.append("    officeId: ").append(toIndentedString(officeId)).append("\n");
         sb.append("    timeseriesId: ").append(toIndentedString(timeseriesId)).append("\n");
         sb.append("    tsCode: ").append(toIndentedString(tsCode)).append("\n");
         sb.append("    aliasId: ").append(toIndentedString(aliasId)).append("\n");
